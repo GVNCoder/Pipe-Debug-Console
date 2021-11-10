4abec8faaa0e05a388e6d98edf7dc2fd0059153e
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-11-10 15:23:24 +0200

    Create a solution

---
+++ Pipe-Debug-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pipe_Debug_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
